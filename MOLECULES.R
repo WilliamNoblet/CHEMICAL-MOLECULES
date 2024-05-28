bc4857c67e0aae4af337bc3c9c2f4ba9f3d68bac
Will Jia <william.noblet.edu@groupe-gema.com>
2024-05-28 23:00:48 +0200

    update

---
+++ MOLECULES.R
@@ -0,0 +1,40 @@
+#MOLECULES
+
+acides_amines <- data.frame(
+  Nom = c("Alanine", "Cystéine", "Aspartate", "Glutamate", "Phénylalanine"),
+  Formule = c("C3H7NO2", "C3H7NO2S", "C4H7NO4", "C5H9NO4", "C9H11NO2")
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
